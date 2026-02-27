64cb79545ea13b947ff263e713251d5edc90bc3a
HeWhoCooked <bintangstudent972@gmail.com>
2026-02-27 21:30:49 +0700

    Add files via upload

---
+++ pertemuan1/Pertemuan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan1;
+
+/**
+ *
+ * @author stevedownes
+ */
+public class Pertemuan1 {
+
+    public static void main(String[] args) {
+        Sepeda sepedaku = new Sepeda();
+        
+        sepedaku.warna = "Abu-abu";
+        sepedaku.tahunProduksi = 2020;
+        System.out.println("Kring kring sepedaku...");
+        System.out.println("Sepedaku berwarna: " + sepedaku.warna);
+        System.out.println("Diproduksi tahun: " + sepedaku.tahunProduksi);
+    }
+}
